d0193f58f7d4192366bf972bc7de8f2f675e31f5
SnehaSSP <theeggcrunch@gmail.com>
2024-03-12 21:54:39 +0530

    This is my pytest marker file modified with regression

--- latestPythonPractice/test_hello.py
+++ latestPythonPractice/test_hello.py
@@ -1,17 +1,24 @@
 import pytest
 
-
 pytestmark = [pytest.mark.fe, pytest.mark.slow]
+
 
 @pytest.mark.smoke
 def test_assert():
     result = 1 + 2
     assert result == 3
 
+
 @pytest.mark.regression
 def test_true():
     assert True
 
+
 @pytest.mark.slow
-def test_falseo():
-    assert False, "This assertion will fail"
+def test_true1():
+    assert True
+
+
+@pytest.mark.regression
+def test_regression():
+    assert True
